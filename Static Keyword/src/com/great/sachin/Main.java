286e0bb0b71667b94b14dc541c5c63125798bef1
Sachin <barpandasachin@gmail.com>
2018-12-02 00:07:10 +0530

    Static in main class

--- Static Keyword/src/com/great/sachin/Main.java
+++ Static Keyword/src/com/great/sachin/Main.java
@@ -1,12 +1,22 @@
 package com.great.sachin;
 
 public class Main {
+
+    public static int multiplier=9;
+
     public static void main(String[] args) {
 
-        StaticTest static1=new StaticTest("Sam");
-        System.out.println(static1.getName()+"has increased by"+StaticTest.getNumInstance());
+//        StaticTest static1=new StaticTest("Sam");
+//        System.out.println(static1.getName()+" has increased by"+StaticTest.getNumInstance());
+//
+//        StaticTest static2=new StaticTest("Sara");
+//        System.out.println(static2.getName()+" has increasedby"+StaticTest.getNumInstance());
+//
+            int answer = multiply(45);
+        System.out.println("The number multiplied is "+ answer);
+    }
 
-        StaticTest static2=new StaticTest("Sara");
-        System.out.println(static2.getName()+"has increasedby"+StaticTest.getNumInstance());
+    public static int multiply(int x){
+        return x * multiplier;
     }
 }
